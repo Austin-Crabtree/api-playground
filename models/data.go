f3c66ed88c7fae6b52d4a2709cfef72623f3960f
Austin Crabtree <crabbyinatree@protonmail.com>
2020-07-24 16:22:05 -0400

    Added additional comments

--- models/data.go
+++ models/data.go
@@ -7,10 +7,10 @@ import (
 
 // Data is a mockup struct of what data would look like
 type Data struct {
-	Id 		int 		`json:"id"`
-	Version int 		`json:"version"`
-	Columns []string 	`json:"columns"`
-	Data 	[][]int 	`json:"data"`
+	Id      int      `json:"id"`
+	Version int      `json:"version"`
+	Columns []string `json:"columns"`
+	Data    [][]int  `json:"data"`
 }
 
 // Fill a Data struct with random data
@@ -18,11 +18,13 @@ func (d *Data) FillRandom() {
 	d.Id = rand.Intn(10)
 	d.Version = rand.Intn(20)
 	numColumns := rand.Intn(25)
+	// Generate random column names
 	for i := 0; i < numColumns; i++ {
 		lenString := rand.Intn(20)
 		d.Columns = append(d.Columns, utils.RandStringBytes(lenString))
 	}
 	numRows := rand.Intn(25)
+	// Generate random data for 2d data field
 	for i := 0; i < numColumns; i++ {
 		var row []int
 		for j := 0; j < numRows; j++ {
@@ -31,5 +33,3 @@ func (d *Data) FillRandom() {
 		d.Data = append(d.Data, row)
 	}
 }
-
-
